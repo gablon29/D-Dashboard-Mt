15e096b583ab024927ac1c5c976f70ea00f99c9e
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-08-25 09:04:38 -0300

    incluyendo el evento para cambiar de pagina

--- src/components/TabIndex.jsx
+++ src/components/TabIndex.jsx
@@ -1,18 +1,44 @@
-import { Tab, TabGroup, TabList } from "@tremor/react";
-import { UserGroupIcon, UserIcon } from "@heroicons/react/solid";
+import { Tab, TabGroup, TabList, Card } from "@tremor/react";
+import { UserIcon, UserGroupIcon } from "@heroicons/react/solid";
+import { useState } from "react";
 
 function TabIndex() {
+  const [selectState, setSelectState] = useState(1);
+  const handleSelect = (evento) => {
+    setSelectState(evento.target.value);
+    console.log(evento.target.value);
+  };
+
   return (
-    <TabGroup>
-      <TabList>
-        <Tab value={1} icon={UserGroupIcon}>
-          Texto uno
-        </Tab>
-        <Tab value={2} icon={UserIcon}>
-          Texto dos
-        </Tab>
-      </TabList>
-    </TabGroup>
+    <>
+      <TabGroup>
+        <TabList className="mb-5" defaultValue={selectState}>
+          <Tab
+            value={1}
+            icon={UserGroupIcon}
+            onClick={(value) => handleSelect(value)}
+          >
+            Texto uno
+          </Tab>
+          <Tab
+            value={2}
+            icon={UserIcon}
+            onClick={(value) => handleSelect(value)}
+          >
+            Texto dos
+          </Tab>
+        </TabList>
+      </TabGroup>
+      {selectState === 1 ? (
+        <Card>
+          <div className="h-28 bg-emerald-300"></div>
+        </Card>
+      ) : (
+        <Card>
+          <div className="h-28 bg-blue-700"></div>
+        </Card>
+      )}
+    </>
   );
 }
 
